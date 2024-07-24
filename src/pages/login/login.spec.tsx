99e8beeafe0930c14900ab3435bf7556b8fe6e79
mzfirozuddin <mz.firozuddin@gmail.com>
2024-07-25 00:46:34 +0530

    login component test

---
+++ src/pages/login/login.spec.tsx
@@ -0,0 +1,23 @@
+import { it, describe, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import LoginPage from "./login";
+// import
+
+describe("Login page", () => {
+  it("should render with required fields", () => {
+    render(<LoginPage />);
+    expect(screen.getByText("Sign in")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Username")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Login" })).toBeInTheDocument();
+    expect(
+      screen.getByRole("checkbox", { name: "Remember me" })
+    ).toBeInTheDocument();
+    expect(screen.getByText("Forgot password")).toBeInTheDocument();
+  });
+});
+
+//* NOTE:
+//* getBy -> Throws an error
+//* findBy -> async
+//* queryBy -> same as "getBy" but don't throw any error. If content not found then return "null"
