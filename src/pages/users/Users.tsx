325493a3042e16f314089bedd7034450c55236e7
mzfirozuddin <mz.firozuddin@gmail.com>
2024-09-28 22:52:38 +0530

    add user table

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,15 +1,85 @@
-import { Breadcrumb } from "antd";
+import { Breadcrumb, Space, Table, Tag } from "antd";
 import { RightOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getUser } from "../../http/api";
+import { User } from "../../types";
+
+const columns = [
+  {
+    title: "First Name",
+    dataIndex: "firstName",
+    key: "firstName",
+    render: (_text: string, record: User) => {
+      return (
+        <div>
+          {record.firstName} {record.lastName}
+        </div>
+      );
+    },
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+    render: (text: string) => {
+      let color = text === "admin" ? "green" : "geekblue";
+      if (text === "customer") {
+        color = "orange";
+      }
+      return (
+        <Tag color={color} key={text}>
+          {text}
+        </Tag>
+      );
+    },
+  },
+
+  {
+    title: "Email Id",
+    dataIndex: "email",
+    key: "email",
+  },
+];
 
 const Users = () => {
+  const {
+    data: users, //: Alise name
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: () => {
+      return getUser().then((res) => res.data);
+    },
+  });
+
   return (
     <>
-      <Breadcrumb
-        // separator=">"
-        separator={<RightOutlined />}
-        items={[{ title: <Link to="/">Dashboard</Link> }, { title: "Users" }]}
-      />
+      <Space direction="vertical" size="large" style={{ width: "100%" }}>
+        <Breadcrumb
+          // separator=">"
+          separator={<RightOutlined />}
+          items={[{ title: <Link to="/">Dashboard</Link> }, { title: "Users" }]}
+        />
+
+        {isLoading && <div>Loading...</div>}
+        {isError && <div>{error.message}</div>}
+
+        <Table columns={columns} dataSource={users} />
+      </Space>
+
+      {/* {users && (
+        <div>
+          <h1>Users</h1>
+          <ul>
+            {users.map((user: User) => (
+              <li>{user.firstName}</li>
+            ))}
+          </ul>
+        </div>
+      )} */}
     </>
   );
 };
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,12 @@ export type Credentials = {
   email: string;
   password: string;
 };
+
+export type User = {
+  id: number;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: string;
+  createdAt: string;
+};
