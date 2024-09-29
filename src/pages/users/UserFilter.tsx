ccc39df646aeb59b0f26c2b90e33e1ee3f30d83d
mzfirozuddin <mz.firozuddin@gmail.com>
2024-09-29 20:51:58 +0530

    add user filter interactivity

--- src/pages/users/UserFilter.tsx
+++ src/pages/users/UserFilter.tsx
@@ -1,20 +1,31 @@
 import { PlusOutlined } from "@ant-design/icons";
 import { Button, Card, Col, Input, Row, Select } from "antd";
 
-const UserFilter = () => {
+type UserFilterProps = {
+  onFilterChange: (filterName: string, filterValue: string) => void;
+};
+
+const UserFilter = ({ onFilterChange }: UserFilterProps) => {
   return (
     <Card>
       <Row justify="space-between">
         <Col span={14}>
           <Row gutter={20}>
             <Col span={10}>
-              <Input.Search placeholder="Search" />
+              <Input.Search
+                placeholder="Search"
+                allowClear={true}
+                onChange={(e) => onFilterChange("searchFilter", e.target.value)}
+              />
             </Col>
             <Col span={7}>
               <Select
                 style={{ width: "100%" }}
                 allowClear={true}
                 placeholder="Select role"
+                onChange={(selectedItem) =>
+                  onFilterChange("roleFilter", selectedItem)
+                }
               >
                 <Select.Option value="admin">Admin</Select.Option>
                 <Select.Option value="manager">Manager</Select.Option>
@@ -26,6 +37,9 @@ const UserFilter = () => {
                 style={{ width: "100%" }}
                 allowClear={true}
                 placeholder="Status"
+                onChange={(selectedItem) =>
+                  onFilterChange("statusFilter", selectedItem)
+                }
               >
                 <Select.Option value="ban">Ban</Select.Option>
                 <Select.Option value="active">Active</Select.Option>
