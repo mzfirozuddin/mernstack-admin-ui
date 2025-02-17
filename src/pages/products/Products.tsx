74bc42b5940eb589afdc9c69756e05c557f23b56
mzfirozuddin <mz.firozuddin@gmail.com>
2025-02-18 00:10:03 +0530

    add Breadcrumb, router

---
+++ src/pages/products/Products.tsx
@@ -0,0 +1,24 @@
+import { RightOutlined } from "@ant-design/icons";
+import { Breadcrumb, Flex, Space } from "antd";
+import { Link } from "react-router-dom";
+
+const Products = () => {
+  return (
+    <>
+      <Space direction="vertical" size="large" style={{ width: "100%" }}>
+        <Flex justify="space-between">
+          <Breadcrumb
+            // separator=">"
+            separator={<RightOutlined />}
+            items={[
+              { title: <Link to="/">Dashboard</Link> },
+              { title: "Products" },
+            ]}
+          />
+        </Flex>
+      </Space>
+    </>
+  );
+};
+
+export default Products;
